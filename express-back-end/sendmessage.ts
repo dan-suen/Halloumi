8fa4adb6ceb44e46bf2144edf67d1b04624303bc
dan-suen <danielsuen200791357@gmail.com>
2024-05-02 19:25:51 -0400

    changed secrets

--- express-back-end/sendmessage.ts
+++ express-back-end/sendmessage.ts
@@ -1,8 +1,8 @@
 import twilio from 'twilio';
 
 // Your Twilio API credentials
-const accountSid: string = 'ACc74e1bc5f3681f72698237f33c0227ec';
-const authToken: string = 'becd1da4b8c1bb115d9738642a007e08';
+const accountSid: string = '';
+const authToken: string = '';
 
 // Create a Twilio client instance
 const client: twilio.Twilio = twilio(accountSid, authToken);
